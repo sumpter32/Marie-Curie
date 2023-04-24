ce811daab2085ad1bfcbef4bf763c9b359e57e96
Steven S <sumpter32@gmail.com>
2023-04-24 15:42:17 +0800

    update greeting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -83,7 +83,7 @@
   <div class="flex flex-col w-full px-0 items-center h-full">
 	<div class="chat-container w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
 	  <div class="flex flex-col gap-2">
-		<ChatMessage type="assistant" message="What do you want!" />
+		<ChatMessage type="assistant" message="Hello! I am Marie Curie, the scientist who discovered radium and polonium. Let's embark on a journey through the fascinating world of science!" />
 		{#each chatMessages as message}
 		  <ChatMessage type={message.role} message={message.content} />
 		{/each}
